ce216cf59502cd57c35e3733116275e4927f5f4d
Satnasiagian <168347461+Satnasiagian@users.noreply.github.com>
2025-07-04 21:10:02 +0800

    Add files via upload

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\AndreasPanelProvider::class,
+    App\Providers\Filament\LinggomPanelProvider::class,
+];
